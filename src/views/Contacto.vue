eb096199e68de7f96288c2f39d71c1ad762d5713
Josia <josiassmith005@gmail.com>
2021-01-06 14:24:04 +0100

    Formulario|Contactos-Incompleto

--- src/views/Contacto.vue
+++ src/views/Contacto.vue
@@ -6,20 +6,35 @@
         <div class="col-12 ContactoTitulo">
           <h1>CONTACTOS</h1>
         </div>
-        <div class="col-6">
+        <div class="col-6" id="Formulario">
           <form action="">
             <div class="row">
               <div class="col-6">
                 <div class="form-group">
-                    <label for="Nome">Nome</label>
-                    <input type="text" class="form-control" name="Nome">
+                  <label for="Nome">Nome</label>
+                  <input type="text" class="form-control" name="Nome" />
                 </div>
               </div>
               <div class="col-6">
                 <div class="form-group">
-                    <label for="Nome">Nome</label>
-                    <input type="text" name="Nome" class="form-control">
+                  <label for="email">E-mail</label>
+                  <input type="email" name="email" class="form-control" />
                 </div>
+              </div>
+              <div class="col-12">
+                <div class="form-group">
+                  <label for="Telefone">Telefone</label>
+                  <input type="tel" name="Telefone" class="form-control" />
+                </div>
+              </div>
+              <div class="col-12">
+                <div class="form-group">
+                  <label for="Mensagem">Mensagem</label>
+                  <input type="text-area" name="Mensagem" class="form-control" />
+                </div>
+              </div>
+              <div class="col-6">
+                <button type="submit" class="btn btn-default btn-lg">ENVIAR MENSAGEM</button>
               </div>
             </div>
           </form>
@@ -55,11 +70,23 @@ export default {
   text-align: left;
   width: 100%;
 }
-label{
-    font-size: 18px;
-    text-align: left!important;
+label {
+  font-size: 20px;
+  text-align: left !important;
+  font-family: "Gotham";
+  color: #707070;
 }
-.form-control{
-    width: 60%;
+#Formulario {
+  padding: 5%;
+}
+.form-control {
+  width: 100%;
+}
+.form-group {
+  text-align: left !important;
+}
+.btn{
+  color: #fff;
+  background-color: #8CB560;
 }
 </style>
